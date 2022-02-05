a9dfec4812c8923bf5ddeb74ab6418e6092b89dc
rabea <rabeamahmood99@gmail.com>
2022-02-05 19:48:36 +0200

    added router to site

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import { SignIn } from './components/authentication/SignIn';
 import { SignUp } from './components/authentication/SignUp';
-import { Store } from './components/Store/Store';
+import { Site } from './components/Site/Site';
 import { ResetPassword } from './components/authentication/ResetPassword';
 
 
@@ -13,7 +13,7 @@ ReactDOM.render(
     <Routes>
       <Route path="/" element={<SignIn />} />
       <Route path="/SignUp" element={<SignUp />} />
-      <Route path="/Store" element={<Store />} />
+      <Route path="/Site" element={<Site />} />
       <Route path="/ResetPassword" element={<ResetPassword />} />
 
     </Routes>
