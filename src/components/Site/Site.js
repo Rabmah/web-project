93151ad0682d927bdd8707918156996c59942c13
rabea <rabeamahmood99@gmail.com>
2022-02-09 15:50:03 +0200

    site-adding cards

--- src/components/Site/Site.js
+++ src/components/Site/Site.js
@@ -16,6 +16,8 @@ import Avatar from '@mui/material/Avatar';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 
+import { Card, CardContent, Grid, Stack } from "@mui/material";
+
 const pages = ['Hotels', 'Restaurants', 'Cars', 'Things to do'];
 //hotels צימרים
 // Cars cars to rent/ bus stations
@@ -23,54 +25,55 @@ const pages = ['Hotels', 'Restaurants', 'Cars', 'Things to do'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 async function handleSite(setopen, seterror) {
-    try {
-        await firebaseAuth.signOut();
-        setopen(true);
-        seterror("Logout Success");
-    }
-    catch (e) {
-        setopen(true);
-        seterror(e.code);
-    }
+  try {
+    await firebaseAuth.signOut();
+    setopen(true);
+    seterror("Logout Success");
+  }
+  catch (e) {
+    setopen(true);
+    seterror(e.code);
+  }
 }
 
 export function Site(props) {
-    const [errors, setErrors] = useState();
-    const [isopen, setIsopen] = useState(false);
-    const [anchorElNav, setAnchorElNav] = React.useState(null);
-    const [anchorElUser, setAnchorElUser] = React.useState(null);
-  
-    const handleOpenNavMenu = (event) => {
-      setAnchorElNav(event.currentTarget);
-    };
-    const handleOpenUserMenu = (event) => {
-      setAnchorElUser(event.currentTarget);
-    };
-  
-    const handleCloseNavMenu = () => {
-      setAnchorElNav(null);
-    };
-  
-    const handleCloseUserMenu = () => {
-      setAnchorElUser(null);
-    };
+  const [errors, setErrors] = useState();
+  const [isopen, setIsopen] = useState(false);
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-    let navigate = useNavigate();
-    useEffect(() => {
-        return firebaseAuth.onAuthStateChanged(u => {
-            if (u) {
-                // yes user
-                // navigate('/Site');
-            } else {
-                // no user
-                navigate("/");
-            }
-        })
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  let navigate = useNavigate();
+  useEffect(() => {
+    return firebaseAuth.onAuthStateChanged(u => {
+      if (u) {
+        // yes user
+        // navigate('/Site');
+      } else {
+        // no user
+        navigate("/");
+      }
     })
-    return (
-        <div>
-            <AppBar>
-            <Container maxWidth="xl">
+  })
+  return (
+    <div>
+      <div>
+        <AppBar>
+          <Container maxWidth="xl">
             <Toolbar disableGutters>
               <Typography
                 variant="h6"
@@ -78,9 +81,9 @@ export function Site(props) {
                 component="div"
                 sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
               >
-                LOGO
+                TRAVIL
               </Typography>
-    
+
               <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
                 <IconButton
                   size="large"
@@ -123,7 +126,7 @@ export function Site(props) {
                 component="div"
                 sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
               >
-                LOGO
+                TRAVIL
               </Typography>
               <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
                 {pages.map((page) => (
@@ -136,7 +139,7 @@ export function Site(props) {
                   </Button>
                 ))}
               </Box>
-    
+
               <Box sx={{ flexGrow: 0 }}>
                 <Tooltip title="Open settings">
                   <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
@@ -168,13 +171,95 @@ export function Site(props) {
               </Box>
             </Toolbar>
           </Container>
-            </AppBar>
-            <Button onClick={() => { handleSite(setIsopen, setErrors) }}>Exit</Button>
-            <Snackbar anchorOrigin={{ horizontal: "center", vertical: "top" }} open={isopen}
-                onClose={() => { setIsopen(false) }}
-                autoHideDuration={3000}>
-                <Alert severity="error"> {errors} </Alert>
-            </Snackbar>
-        </div>
-    );
+        </AppBar>
+      </div>
+
+      <div>
+        <Grid container justifyContent={"center"} mt={"20vh"} alignItems={"center"}>
+          <Grid item md={8}>
+            <Card style={{ backgroundColor: "white" }} variant="elevation">
+              <CardContent>
+                <Stack spacing={2}>
+                  <h2>Here Are the Hotels</h2>
+                  <div>
+                    <p>For a relaxing travil you have to get a good hotels.</p>
+                    <p>here we show you the most wanted hotels in Golan Hieght.</p>
+                    click here t see the hotels
+                  </div>
+                </Stack>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </div>
+
+      <div>
+        <Grid container justifyContent={"center"} mt={"10vh"} alignItems={"center"}>
+          <Grid item md={8}>
+            <Card style={{ backgroundColor: "white" }} variant="elevation">
+              <CardContent>
+                <Stack spacing={2}>
+                  <h2>Restaurants</h2>
+                  <div>
+                    <p>For a good meal you can check this Restaurants to decide where you want to eat.</p>
+                    <p>You can take the full time and don't waste your time to check if you
+                      want this restaurant.</p>
+                    <p>click to see the restaurants that give you the best meal you want.</p>
+                  </div>
+                </Stack>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </div>
+
+      <div>
+        <Grid container justifyContent={"center"} mt={"10vh"} alignItems={"center"}>
+          <Grid item md={8}>
+            <Card style={{ backgroundColor: "white" }} variant="elevation">
+              <CardContent>
+                <Stack spacing={2}>
+                  <h2>Cars / Bus</h2>
+                  <div>
+                    <p>habeb ane mareftsh sho bde aktob hon f ant zabt ha alshe</p>
+                  </div>
+                </Stack>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </div>
+
+      <div>
+        <Grid container justifyContent={"center"} mt={"10vh"} alignItems={"center"}>
+          <Grid item md={8}>
+            <Card style={{ backgroundColor: "white" }} variant="elevation">
+              <CardContent>
+                <Stack spacing={2}>
+                  <h2>Things To Do</h2>
+                  <div>
+                    <p>In Golan Hieght many people do not know somethings they can do.
+                      they know to come in winter to play ski and in spring to see the
+                      cherry and apple crops.</p>
+                    <p>But they do not know what they can do in all the time of the year.</p>
+                    <p>For this we chose to you some places and tracks you can do</p>
+                    <p>check here</p>
+                  </div>
+                </Stack>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </div>
+
+      <div>
+        <Button onClick={() => { handleSite(setIsopen, setErrors) }}>Exit</Button>
+        <Snackbar anchorOrigin={{ horizontal: "center", vertical: "top" }} open={isopen}
+          onClose={() => { setIsopen(false) }}
+          autoHideDuration={3000}>
+          <Alert severity="error"> {errors} </Alert>
+        </Snackbar>
+      </div>
+    </div>
+  );
 }
