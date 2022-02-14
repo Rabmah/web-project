3206af18bb93b49b0da23c1a7430d592c74f65ee
jameel98 <jameelmograbi@gmail.com>
2022-02-14 21:32:37 +0200

    working on hotel page

--- src/components/Site/HotelCard.js
+++ src/components/Site/HotelCard.js
@@ -1,13 +1,67 @@
-import { Stack } from "@mui/material";
+import React from "react";
+import { Stack, Grid, Card, CardContent } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
+const hotels = [
+    {
+        photo: process.env.PUBLIC_URL + "/assets/legacyVillage.PNG",
+        title: "Legacy village hotel",
+        contactInfo: "Email: legacyvillages@gmail.com \n phone number: 0526251435 \n 0542106356 \n 046870173        ",
+        location: "Majdel shams",
+        site: "http://cart-i.com/legacy-village/index.html",
+        services: "באפשרותכם ליהנות מארוחת בוקר כפרית עשירה ומגוונת המוגשת היישר לחדרי האירוח - להתחיל את היום עם חיוך ובאנרגיות שיא, כמו גם ארוחות ערב דרוזיות אוותנטיות ומפנקות. בנוסף, ניתן להזמין טיפולי ספא ועיסויים מפנקים ומרגיעים, לגוף ולנפש, הנעשים על ידי צוות מטפלים מקצועי ומנוסה. בתאום מראש ובתוספת תשלום. ",
+    },
+    {
+        photo: process.env.PUBLIC_URL + "/assets/layarPark.jpeg",
+        title: "Layar park",
+        contactInfo: "phone number: 0505129257 \n 0507769259 ",
+        location: "Ein Kenia",
+        site: "",
+        services: "",
+    },
+
+    {
+        photo: process.env.PUBLIC_URL + "/assets/golanResort.jpeg",
+        title: "golan resort",
+        contactInfo: "phone number: 0504422062 ",
+        location: "Bokaata",
+        site: "",
+        services: "",
+    },
+
+    {
+        photo: process.env.PUBLIC_URL + "/assets/OldSchool.jpg",
+        title: "Old School",
+        contactInfo: "phone number: 0527283622 ",
+        location: "Ein kenia",
+        site: "https://oldschool-zimmer.com/",
+        services: "",
+    },
+];
 export function HotelCard(props) {
+    let navigate = useNavigate();
+
+    const handlePages = (lnk) => {
+        navigate(lnk);
+
+    }
     return (
         <div>
-            <Stack spacing={2}>
-                <h2>Welcome To Our Site</h2>
-                <p>If you not have an account please</p>
-                <p>If you forget your password</p>
-            </Stack>
+            {hotels.map((hotels, i) => (
+                <Grid key={i} container justifyContent={"center"} mt={"10vh"} alignItems={"center"}>
+                    <Grid sx={{ width: '100%', boxShadow: 3 }} item md={8}>
+                        <Card style={{ backgroundImage: `url(${hotels.photo})` }} variant="elevation"
+                            onClick={() => handlePages(hotels.site)}>
+                            <CardContent sx={{ fontStyle: 'oblique' }}>
+                                <Stack spacing={2}>
+                                    <h2>{hotels.title}</h2>
+
+                                </Stack>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                </Grid>
+            ))}
         </div>
     );
 }
--- src/components/Site/Hotels.js
+++ src/components/Site/Hotels.js
@@ -1,28 +1,15 @@
 import React from "react";
-import { Stack, Grid, Card, CardContent } from "@mui/material";
 import { NavBar } from "./AppBar";
 import { pages } from "./Site";
 import Footer from "./Footer";
+import { HotelCard } from "./HotelCard";
 
 export function Hotels(props) {
+
     return (
         <div>
             <NavBar pages={pages}></NavBar>
-            <div>
-                <Grid container justifyContent={"center"} minHeight={"100vh"} alignItems={"center"}>
-                    <Grid item md={4}>
-                        <Card style={{ backgroundColor: "white" }} variant="elevation">
-                            <CardContent>
-                                <Stack spacing={2}>
-                                    <h2>Welcome To Our Site</h2>
-                                    <p>If you not have an account please</p>
-                                    <p>If you forget your password</p>
-                                </Stack>
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                </Grid>
-            </div>
+            <HotelCard />
             <Footer />
         </div>
     );
